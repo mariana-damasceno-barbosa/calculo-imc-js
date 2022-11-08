f885ea3df31ec27327160ee36989e818eadacaf3
Mariana Damasceno Barbosa <marianadamascenob@gmail.com>
2022-11-08 20:44:26 -0300

    commit inicial

---
+++ Assets/js/teste.js
@@ -0,0 +1,10 @@
+function inicio(){
+    inteiro numero, atual = 1, valor = 1
+    numero = 6
+
+    enquanto (atual <= numero){
+        valor = valor*atual
+        atual=atual+1
+    }
+escreva{valor}
+}
